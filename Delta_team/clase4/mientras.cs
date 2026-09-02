eda8d0f09470453d5d0b16d09dfd1254c6d27862
tatianafrias018-maker <tatianafrias018@gmail.com>
2026-09-01 22:40:12 -0400

    wd
    gf

---
+++ Delta_team/clase4/mientras.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delta_team.clase4
+{
+    internal class mientras
+    {
+        public static void Main(string[] args)
+        {
+            int vidas = 0;
+
+            while (vidas > 0)
+            {
+                Console.WriteLine("te queda una vida");
+                vidas--;
+            }
+        }
+    }
+}
